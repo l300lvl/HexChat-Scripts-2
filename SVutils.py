1f7aa502167c759327f3e42ba75758bc4d0ea1b8
undeadrevo <sammael@me.com>
2015-03-11 05:31:27 +0100

    Added a shakespearean insult generator to SVutils.py

--- SVutils.py
+++ SVutils.py
@@ -393,6 +393,42 @@ def storechan(word, word_eol, userdata):
 	global chan
 	chan = word[3]
 	savemebrother()
+c0 = ['filthy', 'dirty', 'stinking']
+
+c1 = ['artless', 'bawdy', 'beslubbering', 'bootless', 'churlish', 'cockered',
+'clouted', 'craven', 'currish', 'dankish', 'dissembling', 'droning', 'errant',
+'fawning', 'fobbing', 'froward', 'frothy', 'gleeking', 'goatish', 'gorbellied',
+'impertinent', 'infectious', 'jarring', 'loggerheaded', 'lumpish', 'mammering',
+'mangled', 'mewling', 'paunchy', 'pribbling', 'puking', 'puny', 'qualling', 
+'rank', 'reeky', 'rogueish', 'ruttish', 'saucy', 'spleeny', 'spongy', 'surly', 
+'tottering', 'unmuzzled', 'vain', 'venomed', 'villainous', 'warped', 'wayward', 
+'weedy','whorish', 'yeastly'] # first column...
+
+c2 = ['base-court', 'bat-fowling', 'beef-witted', 'beetle-headed', 'boil-brained',
+'clapper-clawed', 'clay-brained', 'common-kissing', 'crook-pated', 'dismal-dreaming',
+'dizzy-eyed', 'doghearted', 'dread-bolted', 'earth-vexing', 'elf-skinned', 'fat-kidneyed',
+'fen-sucked', 'flap-mouthed', 'fly-bitten', 'folly-fallen', 'fool-born', 'full-gorged',
+'guts-griping', 'half-faced', 'hasty-witted', 'hedge-born', 'hell-hated', 'idle-headed',
+'ill-breeding', 'ill-nurtured', 'knotty-pated', 'milk-livered', 'motley-minded', 'onion-eyed',
+'plume-plucked', 'pottle-deep', 'pox-marked', 'reeling-ripe', 'rough-hewn', 'rude-growing',
+'rump-fed', 'shard-borne', 'sheep-biting', 'spur-galled', 'swag-bellied', 'tardy-gaited',
+'tickle-brained', 'toad-spotted', 'unchin-snouted', 'weather-bitten'] # second column...
+
+c3 = ['apple-john', 'baggage', 'barnacle', 'bladder', 'boar-pig', 'bugbear', 'bum-bailey',
+'canker-blossom', 'clack-dish', 'clotpole', 'coxcomb', 'codpiece', 'death-token', 'dewberry',
+'flap-dragon', 'flax-wench', 'flirt-gill', 'foot-licker', 'fustilarian', 'giglet', 'gudgeon',
+'haggard', 'harpy', 'hedge-pig', 'horn-beast', 'hugger-mugger', 'joithead', 'lewdster', 'lout',
+'maggot-pie', 'malt-worm', 'mammet', 'measle', 'minnow', 'miscreant', 'moldwarp', 'mumble-news',
+'nut-hook', 'pidgeon-egg', 'pignut', 'puttock', 'pumpion', 'ratsbane', 'scut', 'skainsmate',
+'strumpet', 'varlot', 'vassal', 'whey-face', 'wagtail'] # third column
+
+def cmd_insult(word, word_eol, userdata):
+    insult = "thou %s %s %s %s!" %(random.choice(c0), random.choice(c1), random.choice(c2), random.choice(c3))
+    channel = hexchat.get_info("channel")
+    if len(word) < 2:
+        hexchat.command("MSG %s %s" %(channel, insult))
+    else:
+        hexchat.command("MSG %s %s %s" %(channel, word[1], insult))
 	
 hexchat.hook_command('gt', greentext, help="/gt Shows the desired text in the famous greentext form")
 hexchat.hook_command("kick", kickquote, help="/kick Kicks a user.")
@@ -427,5 +463,7 @@ hexchat.hook_command("fb",foo, help="Usage: wp [  n  |  b  |  p  |  s  |  q   ] 
 
 hexchat.hook_server("474", storechan)
 hexchat.hook_server("PONG", resetsavemebrother)
+
+hexchat.hook_command("insult", cmd_insult)
 
 hexchat.emit_print('Notice', __module_name__ + ' [Plugin]', '%s by %s loaded. You are using version %s of the script.' % (__module_name__, __module_author__, __module_version__))
